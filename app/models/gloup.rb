f8b60c9c6e77728853f07b6d159d65a367d17931
kana-chan <bkm0115_y@icloud.com>
2020-02-27 15:48:13 +0900

    delete & generate collect name model
    D-gloup model
    G-group model

--- app/models/gloup.rb
+++
@@ -1,2 +0,0 @@
-class Gloup < ApplicationRecord
-end
--- db/migrate/20200227064156_create_gloups.rb
+++
@@ -1,8 +0,0 @@
-class CreateGloups < ActiveRecord::Migration[5.0]
-  def change
-    create_table :gloups do |t|
-
-      t.timestamps
-    end
-  end
-end
