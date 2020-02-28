c8d2d620e9fd88919c12edb3a596a1f6838a8c49
kana-chan <bkm0115_y@icloud.com>
2020-02-28 16:54:54 +0900

    create group_edit function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+  def index
+  end
+  
   def new
     @group = Group.new
     @group.users << current_user
@@ -14,13 +17,20 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
-  
+
 private 
 def group_params
-  params.require(:group).permit(:name,user_id:[])
+  params.require(:group).permit(:name,user_ids:[])
 end
 end
