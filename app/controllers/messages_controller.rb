3a85a4f6648a62f830874229a02f31f672a2aa85
kana-chan <bkm0115_y@icloud.com>
2020-03-04 14:21:00 +0900

    define index action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,13 @@
 class MessagesController < ApplicationController
   before_action :set_group
   def set_group
-    @group = Group.find(params[:id])
+    @group = Group.find(params[:group_id])
   end
 
-
+# resources
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
