78ec8ac3cd7375c7f8f6ffb152e17524f72c68c5
kana-chan <bkm0115_y@icloud.com>
2020-03-04 15:46:10 +0900

    fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validates :content, presences: true, unless: :image?
+  validates :content, presence: true, unless: :image?
 
-  mount_uploader :image, ImageUpLoader
+  mount_uploader :image, ImageUploader
 end
