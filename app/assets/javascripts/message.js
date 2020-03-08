90d7ce34baf48648d4cf77e24d7be73582939743
kana-chan <bkm0115_y@icloud.com>
2020-03-08 22:25:21 +0900

    send formdata

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,15 @@
 $(function(){
   $('#new_message').on('submit',function(e){
     e.preventDefault()
+    var url = $(this).attr('action');
+    var formData = new FormData(this);
+    $.ajax({
+      url:url,
+      data:formData,
+      type: "POST",
+      dataType:'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
